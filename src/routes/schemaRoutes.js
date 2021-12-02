413e4b4ca637ed7adba583f01e7eb9e187b383a9
Tom Cahill <me@tomcahill.co.uk>
2021-12-02 19:36:16 +0000

    TWEAK: Allow users to bulk add

--- src/routes/schemaRoutes.js
+++ src/routes/schemaRoutes.js
@@ -365,7 +365,7 @@ class AddMany extends Route {
 	constructor(schema, appShort) {
 		super(`${schema.name}/bulk/add`, `BULK ADD ${schema.name}`);
 		this.verb = Route.Constants.Verbs.POST;
-		this.auth = Route.Constants.Auth.ADMIN;
+		this.auth = Route.Constants.Auth.USER;
 		this.permissions = Route.Constants.Permissions.ADD;
 
 		this.activityDescription = `BULK ADD ${schema.name}`;
