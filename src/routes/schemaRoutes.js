f2499ac174be300419d3686a5eea44e018a432d8
Tom Cahill <me@tomcahill.co.uk>
2021-12-02 19:37:08 +0000

    TWEAK: Added bulk/update api

--- src/routes/schemaRoutes.js
+++ src/routes/schemaRoutes.js
@@ -492,6 +492,104 @@ class UpdateOne extends Route {
 routes.push(UpdateOne);
 
 /**
+ * @class UpdateMany
+ */
+class UpdateMany extends Route {
+	constructor(schema, appShort) {
+		super(`${schema.name}/bulk/update`, `BULK UPDATE ${schema.name}`);
+		this.verb = Route.Constants.Verbs.POST;
+		this.auth = Route.Constants.Auth.USER;
+		this.permissions = Route.Constants.Permissions.WRITE;
+
+		this.activityDescription = `BULK UPDATE ${schema.name}`;
+		this.activityBroadcast = true;
+
+		let schemaCollection = schema.collection;
+		if (appShort) {
+			schemaCollection = `${appShort}-${schema.collection}`;
+		}
+
+		// Fetch model
+		this.schema = new Schema(schema);
+		this.model = Model[schemaCollection];
+
+		if (!this.model) {
+			throw new Error(`GetList Route missing model ${schemaCollection}`);
+		}
+
+		this._entity = null;
+	}
+
+	_validate(req, res, token) {
+		if (!Array.isArray(req.body)) {
+			this.log(`${this.schema.name}: Expected body to be an array of updates`, Route.LogLevel.ERR, req.id);
+			return Promise.reject(new Helpers.RequestError(400, `${this.schema.name}: Expected body to be an array of updates`));
+		}
+
+		// Reduce down duplicate entity updates into one object
+		return req.body.reduce((reducedUpdates, update) => {
+			retunr 
+		}, []);
+
+		return req.body.reduce((prev, update) => {
+			return prev.then(() => {
+				const validation = this.model.validateUpdate(update.body);
+				if (!validation.isValid) {
+					if (validation.isPathValid === false) {
+						this.log(`${this.schema.name}: Update path is invalid: ${validation.invalidPath}`, Route.LogLevel.ERR, req.id);
+						return update.validation = {
+							code: 400,
+							message: `${this.schema.name}: Update path is invalid: ${validation.invalidPath}`,
+						};
+					}
+					if (validation.isValueValid === false) {
+						this.log(`${this.schema.name}: Update value is invalid: ${validation.invalidValue}`, Route.LogLevel.ERR, req.id);
+						if (validation.isMissingRequired) {
+							return update.validation = {
+								code: 400,
+								message: `${this.schema.name}: Missing required property updating ${req.body.path}: ${validation.missingRequired}`,
+							};
+						}
+
+						return update.validation = {
+							code: 400,
+							message: `${this.schema.name}: Update value is invalid for path ${req.body.path}: ${validation.invalidValue}`,
+						};
+					}
+				}
+
+				return this.model.exists(update.id)
+					.then((exists) => {
+						if (!exists) {
+							this.log('ERROR: Invalid ID', Route.LogLevel.ERR, req.id);
+							return update.validation = {
+								code: 400,
+								message: `${this.schema.name}: Missing required property updating ${req.body.path}: ${validation.missingRequired}`,
+							};
+						}
+
+						return update.validation = true;
+					});
+			});
+		}, Promise.resolve())
+			.then(() => 'foo');
+	}
+
+	_exec(req, res, data) {
+		console.log(validate);
+		const output = [];
+		return req.body.reduce(
+			(prev, body) => prev
+				.then(() => this.model.updateByPath(body.body, body.id))
+				.then((result) => output.push({id: body.id, result: result})),
+			Promise.resolve(),
+		)
+			.then(() => output);
+	}
+}
+routes.push(UpdateMany);
+
+/**
  * @class DeleteOne
  */
 class DeleteOne extends Route {
